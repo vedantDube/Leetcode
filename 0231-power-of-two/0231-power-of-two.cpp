06530af8fe940afa389b56b17def18398202dff1
vedantDube <161755713+vedantDube@users.noreply.github.com>
2025-01-02 00:21:34 +0530

    Time: 0 ms (100%), Space: 8 MB (12.58%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        long long a = n;
-        if(a==0){
-            return false;
+        int ans = 1;
+        for(int i = 0;i<=30;i++){
+            if(ans==n){
+                return true;
+            }
+            if(ans<INT_MAX/2){
+                ans = ans*2;
+            }
         }
-        return !(a && ((a&(a-1))));
+        return false;
     }
 };
