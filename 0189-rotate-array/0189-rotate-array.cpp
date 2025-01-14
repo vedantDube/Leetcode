c2dd9aea1ac8097bc7c0444b39e2693f15ada34e
vedantDube <161755713+vedantDube@users.noreply.github.com>
2025-01-15 00:38:07 +0530

    Time: 1 ms (14.47%), Space: 30.4 MB (14.4%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        ios::sync_with_stdio(false);
-        cin.tie(nullptr);
-        int n = nums.size();
-        reverse(nums.begin(),nums.end());
-        k = k%n;
-        reverse(nums.begin(),nums.begin()+(k));
-        reverse(nums.begin()+k,nums.end());        
+        vector<int> temp(nums.size());
+
+        for(int i = 0;i<nums.size();i++){
+            temp[(i+k)%nums.size()]=nums[i];
+        }
+        nums = temp;
     }
 };
