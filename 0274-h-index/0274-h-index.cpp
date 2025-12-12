9017c40df71ffcbc3543476b1448ff05709056d3
Vedant Dubey <161755713+vedantDube@users.noreply.github.com>
2025-12-13 00:13:27 +0530

    Time: 0 ms (100%), Space: 12.3 MB (64.61%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        sort(citations.begin(), citations.end());
         int n = citations.size();
-        
-        int h = 0;
+        sort(citations.begin(), citations.end());
+
         for (int i = 0; i < n; i++) {
-            int least = n - i; 
-            if (citations[i] >= least) {
-                return least;
+            if (citations[i] >= n - i) {
+                return n - i;
             }
         }
+
         return 0;
     }
 };
